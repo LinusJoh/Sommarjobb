a9563afed8736c6aa346b0a40c150da3f428e76e
Linus Johansson <ljo@apptus.com>
2018-06-05 08:22:15 +0200

    Did a plus function

--- Sommarjobbare.java
+++ Sommarjobbare.java
@@ -2,4 +2,9 @@ public class Sommarjobbare {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    public int plus(int a, int b){
+	return (a+b);
+    }    
 }
+
+
